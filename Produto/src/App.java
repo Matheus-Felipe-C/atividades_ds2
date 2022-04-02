353a141c935bab9e5cd96e88dc5d7da9d78f8bbf
ALUNO <ALUNO@senaiescola.fsa>
2022-04-01 21:30:00 -0300

    Interface linha de comando refeita (WIP)

--- Produto/src/App.java
+++ Produto/src/App.java
@@ -1,35 +1,75 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Produto produto1 = new Produto("Leite em pó", "Ninho", 15.0);
-        Produto produto2 = new Produto("Leite em pó", "Itambé", 15.0);
-        Produto produto3 = new Produto("Leite em pó", "Glória", 1.76);
-
+        Scanner sc = new Scanner(System.in);
         GerenciadorDeProdutos gerenciador = new GerenciadorDeProdutos();
+        String desc = "", marca = "", nDesc = "", nMarca = "";
+        Double preco = 0.0, nPreco = 0.0;
 
-        System.out.println("Adicionando produtos");
-        System.out.println(gerenciador.addProduto(produto1));
-        System.out.println(gerenciador.addProduto(produto2));
-        System.out.println(gerenciador.addProduto(produto3));
+        System.out.println("Olá! Bem vindo!");
+        
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1 - Adicionar um produto\n2 - Consultar um produto\n3 - Atualizar um produto");
+            System.out.println("4 - Remover um produto");
+            int input = sc.nextInt();
 
-        System.out.println("\n");
+            switch (input) {
+                case 1:
+                    System.out.println("Digite a descrição do produto");
+                    desc = sc.next();
+                    System.out.println("Digite a marca do produto");
+                    marca = sc.next();
+                    System.out.println("Digite o preço do produto");
+                    preco = sc.nextDouble();
 
-        System.out.println("Consultando produtos");
-        System.out.println(gerenciador.consultaProduto("Leite em pó", "Ninho"));
-        System.out.println(gerenciador.consultaProduto("Leite em pó", "Itambé"));
-        System.out.println(gerenciador.consultaProduto("Leite de pombo", "PomboINC"));
+                    Produto produto = new Produto(desc, marca, preco);
 
-        System.out.println("\n");
+                    gerenciador.addProduto(produto);
 
-        System.out.println("Removendo produtos");
-        System.out.println(gerenciador.removeProduto("Leite em pó", "Glória"));
-        System.out.println(gerenciador.removeProduto("Leite de pombo", "PomboINC"));
-    
-        System.out.println("\n");
+                    break;
+                
+                case 2:
+                    System.out.println("Digite a descrição do produto");
+                    desc = sc.next();
+                    System.out.println("Digite a marca do produto");
+                    marca = sc.next();
 
-        System.out.println("Atualizando produtos");
-        System.out.println(gerenciador.addProduto(produto3));
-        System.out.println(gerenciador.atualizaProduto("Leite em pó", "Glória", "Leite de pombo", "PomboINC", 0.99));
-        System.out.println(gerenciador.consultaProduto("Leite de pombo", "PomboINC"));
-        System.out.println(gerenciador.atualizaProduto("Leite em pó", "Glória", "Leite em pó", "Piracanjuba", 9.99));
+                    if (gerenciador.consultaProduto(desc, marca) == null) System.out.println("Produto não encontrado");
+                    else System.out.println(gerenciador.consultaProduto(desc, marca));
+
+                    break;
+
+                case 3:
+                    System.out.println("Digite a descrição do produto a ser removida");
+                    desc = sc.next();
+                    System.out.println("Digite a marca do produto a ser removida");
+                    marca = sc.next();
+                    System.out.println("Digite a nova descrição a ser adicionada");
+                    nDesc = sc.next();
+                    System.out.println("Digite a nova marca a ser adicionada");
+                    nMarca = sc.next();
+
+                    System.out.println(gerenciador.atualizaProduto(desc, marca, nDesc, nMarca, nPreco));
+
+                    break;
+
+                case 4:
+                    System.out.println("Digite a descrição do produto a ser removida");
+                    desc = sc.next();
+                    System.out.println("Digite a marca do produto a ser removida");
+                    marca = sc.next();
+
+                    System.out.println(gerenciador.removeProduto(desc, marca));
+
+                    break;
+            
+                default:
+                    System.out.println("Opção inválida, por favor selecione uma opção válida");
+                    break;
+            }
+        }
+        
     }
 }

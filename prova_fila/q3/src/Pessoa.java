0aa756ebf1f03db663ccef9c6651b2b5cdf7d800
ALUNO <ALUNO@LAB-DEV-027.senaiescola.fsa>
2022-05-09 20:53:51 -0300

    Questão 3 concluída

---
+++ prova_fila/q3/src/Pessoa.java
@@ -0,0 +1,38 @@
+public class Pessoa implements Comparable<Pessoa> {
+    private int prioridade;
+    private String nome;
+
+    public Pessoa(int prioridade, String nome) {
+        this.prioridade = prioridade;
+        this.nome = nome;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", prioridade=" + prioridade + "]";
+    }
+    
+    @Override
+    public int compareTo(Pessoa o) {
+        if (this.getPrioridade() > o.getPrioridade()) return 1;
+        if (this.getPrioridade() < o.getPrioridade()) return -1;
+        else return 0;
+    }
+
+}
